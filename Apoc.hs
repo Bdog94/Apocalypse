ae78e24c76e9edae9a85d89a7d8aaedccf5a03ba
Bernie Mayer <bemayer@ucalgary.ca>
2016-02-10 14:27:33 -0700

    Updated the upadate method(sorry :|)

--- Apoc.hs
+++ Apoc.hs
@@ -134,14 +134,12 @@ updateState :: GameState -> Maybe ([(Int,Int)]) -> Maybe ([(Int,Int)]) -> GameSt
 updateState state Nothing Nothing = GameState Passed (blackPen state) Passed (whitePen state) (theBoard state)
 updateState state bMove wMove | (bMove == Nothing) || (wMove == Nothing) = if (bMove == Nothing)
 									  then GameState Passed (blackPen state) (moveType state (fromJust wMove) White) (whitePen state) (handlePlayerMove (theBoard state) (fromJust wMove) White)
-									  else GameState (moveType state (fromJust bMove) Black) (blackPen state) Passed (whitePen state) (handlePlayerMove (theBoard state) (fromJust bMove) Black)
-updateState state bMove wMove = GameState (if bMove == Nothing
-										   then Passed
-										   else Played (head (fromJust bMove), head(tail (fromJust bMove))))
+									  else GameState (moveType state (fromJust bMove) Black) (blackPen state) Passed (whitePen state) (handlePlayerMove (theBoard state) (fromJust bMove) Black)									  									  
+updateState state bMove wMove | isValidMove (theBoard state) (head (fromJust bMove)) (head(tail (fromJust bMove)))
+								&& isValidMove (theBoard state) (head (fromJust wMove)) (head(tail (fromJust wMove)))
+										 = GameState  (Played (head (fromJust bMove), head(tail (fromJust bMove))))
 										   (blackPen state)
-										   (if wMove == Nothing
-										   then Passed
-										   else Played (head (fromJust wMove), head(tail (fromJust wMove))))
+										   (Played (head (fromJust wMove), head(tail (fromJust wMove))))
 										   (whitePen state)
 										   (replace2 (
 										   replace2 (
@@ -154,7 +152,9 @@ updateState state bMove wMove = GameState (if bMove == Nothing
 												   ((fromJust wMove) !! 0) E)
 moveType :: GameState -> ([(Int, Int)]) -> Player -> Played
 
-moveType _ _ _ = Init
+moveType state list p | length list == 1 = UpgradedPawn2Knight (head list)
+moveType state ((x_s, y_s) : (x_d, y_d) : list) p | not(isValidMove (theBoard state) (x_s, y_s) (x_d, y_d))  = Goofed ((x_s, y_s), (x_d, y_d))
+moveType state list p = Played ( (head list), list !! 1)
 
 handlePlayerMove :: Board -> ([(Int, Int)]) -> Player -> Board
 

9aeb32c68ddae2d85b711b4d944e55b3e81da05a
aiwanisz <aiwanisz@ucalgary.ca>
2016-02-20 14:30:31 -0700

    Added haddocks to ApocStrategyHuman, cleaning up code as well

--- ApocStrategyHuman.hs
+++ ApocStrategyHuman.hs
@@ -1,11 +1,17 @@
 module ApocStrategyHuman where
 
+
+
+ --IMPORTS--
+ 
 import Control.Monad
 import Control.Monad.Trans.State.Lazy
 import Data.Maybe (fromJust, isNothing)
 import System.IO.Unsafe
 import ApocTools
 import Data.Char
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
 
 {- | This is just a placeholder for the human strategy: it always chooses to play
      (0,0) to (2,1).
@@ -16,76 +22,42 @@ human b PawnPlacement c = getPlacement
 
 
 
+-- | This is the getMove function, it will take a 4 digit move input and return it in a list, filtering out spaces in the process and ignoring a comment after.
+
 getMove = do
     theMove <- getLine --set input to the input the user enters
     let input = filter(/=' ') theMove
-    if input == [] then do return Nothing 
-    else return (Just[(digitToInt(input!!0), digitToInt(input!!1)), (digitToInt(input!!2), digitToInt(input!!3))])
+    if input == [] then do return Nothing --return nothing if player passes
+    else return (Just[(digitToInt(input!!0), digitToInt(input!!1)), (digitToInt(input!!2), digitToInt(input!!3))])-- return move while converting chars from input to ints
+ 
+ 
    
+-- | This is the getPlacement function, it will take a 2 digit move (for pawn placement) and return it in an array filtering out spaces and comments. 
+
 getPlacement = do
     theMove <- getLine --set input to the input the user enters
-    let input = filter(/=' ') theMove
-    if input == [] then do return Nothing 
+    let input = filter(/=' ') theMove -- filter out spaces
+    if input == [] then do return Nothing -- return nothing if player passes
     else return (Just[(digitToInt(input!!0), digitToInt(input!!1))])
-   
 
 
 
-{-
 
-getGameMode :: IO ()
-getGameMode = do
-	argList <- getArgs
-	evalArgList argList
+-- | This is the getGameMode function, it will input the user for the game strategy of the white and black player.
 
-
-argsNotGiven :: IO ()    
-argsNotGiven = do
-    putStrLn "Possible strategies: \n\thuman\n\tgreedy\nEnter the strategy for BLACK:"
-    blackChoice <-getLine
-    when (blackChoice /= "greedy" && blackChoice /= "human") $ do 
-            putStrLn "  human"
-            putStrLn "  greedy"
-            
-    putStrLn "Enter the strategy for WHITE:"
-    whiteChoice <-getLine
-    when (whiteChoice /= "greedy" && whiteChoice /= "human") $ do 
-            putStrLn "  human"
-            putStrLn "  greedy"
-    putStrLn ">>>"
-    --return list of whiteChoice and blackChoice to main somehow
-
-
-evalArgList :: [String] -> IO ()
-evalArgList [] = argsNotGiven
-evalArgList [x] = argsNotGiven
-evalArgList (x:y:[]) 
- | (validMode x) && (validMode y) = putStrLn "Terminate"
- | otherwise = argsNotGiven
-
-
-validMode :: String -> Bool
-validMode x 
- | x == "human" = True
- | x == "greedy" = True
- | otherwise = False
-
---///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--}
-
-
-
-getGameMode :: IO [String]
+getGameMode :: IO [String] -- ^ returns list with first element being black player strategy and second element being white player strategy.
 getGameMode = do
     putStrLn "Possible strategies: \n  human\n  greedy\nEnter the strategy for BLACK:"
-    blackChoice <-getLine
+    blackChoice <-getLine --assign input to blackChoice
             
     putStrLn "Enter the strategy for WHITE:"
-    whiteChoice <-getLine
+    whiteChoice <-getLine --assign input to whiteChoice
 
     putStrLn ">>>"
     let choices = [blackChoice, whiteChoice]
-    return choices
+    return choices --return list of choices
+
+
 
 
 

9155c4b805a088981923f239312712c4c604de53
Kyle-Perry <perry.246@gmail.com>
2016-02-21 16:03:36 -0700

    Fixed a bug that allowed a player to have more than 2 knights. Haddock documentation added.

--- Apoc.hs
+++ Apoc.hs
@@ -134,9 +134,14 @@ gameLoop state wStrat bStrat | (blackPlay state == Passed) && (whitePlay state =
 								
 																		(putStrLn (getWinnerString state wStrat bStrat))
                              | ((pawn2Upgrade state) == True) && 
-                               (((countPiece (flatten (theBoard state)) BK) 
-                                + (countPiece (flatten (theBoard state)) WK) )< 4) = 
-                                do 
+                               (((countPiece (flatten (theBoard state)) BK)  < 2) && (getPawnPlayer (theBoard state) (getPawn(theBoard state)) == Black))
+                                = do 
+                                    print state
+                                                                                        
+                                    (gameLoop (handlePromotion state (getPawnPlayer (theBoard state) (getPawn(theBoard state)))) wStrat bStrat)
+                             | ((pawn2Upgrade state) == True) && 
+                               (((countPiece (flatten (theBoard state)) WK)  < 2) && (getPawnPlayer (theBoard state) (getPawn(theBoard state)) == White))
+                                = do 
                                     print state
                                                                                         
                                     (gameLoop (handlePromotion state (getPawnPlayer (theBoard state) (getPawn(theBoard state)))) wStrat bStrat)
@@ -285,18 +290,17 @@ pickMove strat state playtype player | strat == "human" = human state playtype p
                                      | strat == "greedy" = greedy state playtype player 
                                      | strat == "random" = randomStrategy state playtype player
                                      | True = return Nothing
-
-
--- Game over conditions
---One of the players looses all his/her pawns.  The other player is the winner. 
---One of the players accumulates two penalty points.  The other player is the winner.
---Both players pass on the same round. The one with the most pawns wins.
--- | This method will return the winner or return Nothing.
--- | Note it is not compatible with testing whether or not both players pass on the same round
--- that functionality is easier to do in the gameLoop I believe
-
-
--- | The isWinner function will take the gameState, and determine of a player has won the game, if he has, it will return that player.
+									 
+{- |									 
+The isWinner function will take the gameState, and determine of a player has won the game, if he has, it will return that player.
+									 
+	Game over conditions:
+	-	One of the players looses all his/her pawns.  The other player is the winner. 
+	-	One of the players accumulates two penalty points.  The other player is the winner.
+	-	Both players pass on the same round. The one with the most pawns wins.
+	-	This method will return the winner or return Nothing.
+	-	Note it is not compatible with testing whether or not both players pass on the same round
+-}
 isWinner :: GameState -- ^ Initial Game State
             -> Maybe Player -- ^ Player who won (or nothing if nobody has won)
 
@@ -342,16 +346,15 @@ isClash (x,y) (w,z) = if ((x == w)&&(y == z))
                       then True
                       else False
 
---Handles the 2 possible clash scenarions
---Knight and Pawn on the same space = Knight destroys Pawn
---2 pieces of the same type on the same space = both pieces destroyed
---Returns the board in the new configuration when done
-
-
-
 --Note: This function performs no error checking (assumes that the moves were valid and that there is a clash)
 
--- | handleClash is a function that will determine the outcome of a clash on the game board.
+{- | handleClash is a function that will determine the outcome of a clash on the game board.
+
+Handles the 2 possible clash scenarions
+	-	Knight and Pawn on the same space = Knight destroys Pawn
+	-	2 pieces of the same type on the same space = both pieces destroyed
+	-	Returns the board in the new configuration when done
+-}
 handleClash :: [(Int, Int)] -- ^ Black Player Move
             -> [(Int, Int)] -- ^ White Player Move
             -> Board -- ^ Initial Game Board
@@ -488,101 +491,32 @@ replace xs n elem = let (ys,zs) = splitAt n xs
 -- | Replaces the (x,y)th element in a list of lists with a new element.
 replace2        :: [[a]] -> (Int,Int) -> a -> [[a]]
 replace2 xs (x,y) elem = replace xs y (replace (xs !! y) x elem)
-
-
--- | Similar to validMovesGenerator but is used in conjunction with the player
-validMovesGenPlayer :: Board -> Player -> [ ((Int, Int), (Int, Int))] -> [ ((Int, Int) , (Int , Int))]
-
-validMovesGenPlayer theBoard player [] = []
-validMovesGenPlayer theBoard player ((source, dest) :xs) = if isValidForPlayer theBoard player ([(source)] ++ [(dest)])
-														   then [(source, dest)] ++ validMovesGenPlayer theBoard player xs
-														   else validMovesGenPlayer theBoard player xs
-
-
---  | This method will determine the list of valid moves
-validMovesGenerator :: Board -> [  ((Int , Int) , (Int , Int))] -> [ ((Int , Int) , (Int , Int) )]
-validMovesGenerator theBoard [] = []
-validMovesGenerator theBoard ((x,y):xs) =  if isValidMove theBoard x y
-                                       then [(x,y)] ++ validMovesGenerator theBoard xs
-                                       else validMovesGenerator theBoard xs
-                                       
-
-formatStringForGreedy :: String -> String 
-
-
-formatStringForGreedy s = filter helperFormatStringForGreedy (removeFront s)
-
--- Removes " _ _ _ _ _\n" from the String
-removeFront :: String -> String 
-
-removeFront (x:xs) | x == '\n'  && length xs  >= 0= [] ++ xs
-removeFront (x:xs) = removeFront xs 
-
-helperFormatStringForGreedy :: Char -> Bool
-helperFormatStringForGreedy '|' = False
-helperFormatStringForGreedy '\n' = False
-helperFormatStringForGreedy  c = True
-
-
-
-
-generateMovesForGreedyStrat :: Board -> [ ((Int, Int), (Int, Int))] 
-
-generateMovesForGreedyStrat b = generateMovesForGreedyStratString (formatStringForGreedy (board2Str b)) 0 0
-
-
-generateMovesForGreedyStratString :: String -> Int  -> Int -> [((Int, Int) , (Int, Int))]
-
-generateMovesForGreedyStratString [] prevLen offset = []
-generateMovesForGreedyStratString (c: cs) prevLen offset|  (prevLen + length(c:cs)) > 29 || (prevLen + length(c:cs)) < 4 = []
-														|  (prevLen) <= 4 = 
-                 generateMovesForGreedyStratUsingChar c ((offset `mod` 5), 0) ++ generateMovesForGreedyStratString cs (prevLen +1) (offset + 1)
-														|  (prevLen) <= 9 = 
-                 generateMovesForGreedyStratUsingChar c ((offset `mod` 5), 1) ++ generateMovesForGreedyStratString cs (prevLen +1) (offset + 1)
-														|  (prevLen) <= 14 = 
-                 generateMovesForGreedyStratUsingChar c ((offset `mod` 5), 2) ++ generateMovesForGreedyStratString cs (prevLen +1) (offset + 1)
-														|  (prevLen ) <= 19 = 
-                 generateMovesForGreedyStratUsingChar c ((offset `mod` 5), 3) ++ generateMovesForGreedyStratString cs (prevLen +1) (offset + 1)
-														|  (prevLen ) <= 24 = 
-                 generateMovesForGreedyStratUsingChar c ((offset `mod` 5), 4) ++ generateMovesForGreedyStratString cs (prevLen +1) (offset + 1)
-                 
-generateMovesForGreedyStratUsingChar :: Char -> (Int, Int) -> [ ((Int, Int), (Int, Int))]
-generateMovesForGreedyStratUsingChar c (x,y) | c == '_' = []
-generateMovesForGreedyStratUsingChar c (x,y) | c == '/' || c == '+' = [ ((x,y), (x, y+1))]++ [ ((x,y), (x -1 , y))] ++ [((x,y), (x+1, y+1))] ++ [((x,y) , (x -1, y +1))] ++
-                                            [((x,y) , (x, y -1 ))] ++ [((x,y), (x - 1, y))] ++ [((x,y) , (x-1, y -1))] ++ [((x,y), (x +1 , y -1))]
-generateMovesForGreedyStratUsingChar c (x,y) | c == '#' || c == 'X' = [ ((x,y), (x+1, y+2))]++ [ ((x,y), (x -1 , y+2))] ++ [((x,y), (x+2, y+1))] ++ [((x,y) , (x -2, y +1))] ++
-                                            [((x,y) , (x+2, y -1 ))] ++ [((x,y), (x - 1, y-2))] ++ [((x,y) , (x-2, y -1))] ++ [((x,y), (x +1 , y -2))]
-
-
-
-
-
-generateMovesForGreedyStrat' :: Board -> (Int, Int) -> [ ((Int, Int), (Int, Int))]
-
-generateMovesForGreedyStrat' _ _ = []
---generateMovesForGreedyStrat' b (x,y) |  (x > 5 || y > 5) || (x < 0 || y < 0) = []
---generateMovesForGreedyStrat' b (x,y) |  (getFromBoard b (x,y)) == E = []
---generateMovesForGreedyStrat' b (x,y) |  (getFromBoard b (x,y)) == WP  || getFromBoard b (x,y) == BP
---                                       = [ ((x,y), (x, y+1)),  ((x,y) , (x +1, y)), ((x, y),(x+1,y+1)) , ((x,y), (x -1, y+1)) 
---                                              ((x,y), (x, y-1)),  ((x,y) , (x -1, y)), ((x, y),(x-1,y-1)) , ((x,y), (x +1, y -1))]
                                               
---Additional check to ensure a player can only move their own piece
+-- | Additional check to ensure a player can only move their own piece
 isValidForPlayer :: Board -> Player -> [(Int,Int)] -> Bool
 isValidForPlayer board player [] = False
 isValidForPlayer board player (first:rest) | (getFromBoard board first) == E = False
 										   | (player == playerOf (pieceOf(getFromBoard board first)))  = isValidMove board first (head rest)
                                            | True = False
                                       
+{- |
+	Checks if a move is valid or not on the game board 
+	A knights move is valid if it:
+		-	moves 2 spaces on one axis, and one space on the other (totaling 3 spaces)
+		-	if the target space is empty or contains an opponent's piece.
+	A pawns move is valid if it:
+		-	moves one space vertically into an open space
+		-	moves diagonally one space onto a space occupied by an opponent's piece
+-}
 
--- | Checks if a move is valid or not on the game board 
 isValidMove :: Board -> (Int,Int) -> (Int,Int) -> Bool
 isValidMove theBoard (x,y) (w,z)
     | (x<0) || (x>4)|| (y<0) || (y>4) || (w<0) || (w>4) || (z<0) || (z>4) = False
-    |((getFromBoard theBoard (x,y)) == WK ) =               --  A knights move is valid if it:
-        if (((abs (x-w))<3) && ((abs (y-z)))<3)             --  moves 2 spaces on one axis, and one space on the other 
-           && (((abs (x-w)) + (abs (y-z))) == 3)            --  (totaling 3 spaces)
-           && (((getFromBoard theBoard (w,z)) ==  E)        --  and if the target space is empty 
-               || ((getFromBoard theBoard (w,z)) ==  BK)    --  or contains an opponent's piece.
+    |((getFromBoard theBoard (x,y)) == WK ) =               
+        if (((abs (x-w))<3) && ((abs (y-z)))<3)             
+           && (((abs (x-w)) + (abs (y-z))) == 3)            
+           && (((getFromBoard theBoard (w,z)) ==  E)       
+               || ((getFromBoard theBoard (w,z)) ==  BK)    
                || ((getFromBoard theBoard (w,z)) ==  BP))
         then True
         else False
@@ -595,11 +529,7 @@ isValidMove theBoard (x,y) (w,z)
                || ((getFromBoard theBoard (w,z)) ==  WP))
         then True
         else False
- --  A pawns move is valid if it:
- --  moves one space vertically into an open space
- --  or move diagonally one space onto a space 
- --  occupied by an opponent's piece
-    |((getFromBoard theBoard (x,y)) == WP ) && (((abs (x-w)) == 0) && ((y-z) == -1) && getFromBoard theBoard (w,z) == E)           
+	|((getFromBoard theBoard (x,y)) == WP ) && (((abs (x-w)) == 0) && ((y-z) == -1) && getFromBoard theBoard (w,z) == E)           
         = True                                           
     |((getFromBoard theBoard (x,y)) == WP ) && (((abs (x-w)) == 1) && ((y-z) == -1)) 
 	&& (((getFromBoard theBoard (w,z)) ==  BK) || ((getFromBoard theBoard (w,z)) ==  BP))
@@ -609,4 +539,4 @@ isValidMove theBoard (x,y) (w,z)
     |((getFromBoard theBoard (x,y)) == BP ) && (((abs (x-w)) == 1) && ((y-z) == 1)) 
 	&& (((getFromBoard theBoard (w,z)) ==  WK) || ((getFromBoard theBoard (w,z)) ==  WP))
              = True
-    |True = False
+    |True = False 
